aaf005226bdd115a86e39561206bd04dc9024861
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2024-03-06 16:59:30 +0530

    question 7

---
+++ Pattern/q7.js
@@ -0,0 +1,15 @@
+let n = 5;
+let m = 9;
+for (let i = 0; i < n; i++) {
+  let str = "";
+  for (let j = 0; j <= m; j++) {
+    if (i == 0 && j == (m - 1) / 2) {
+      str += "*";
+    } else if (j <= (m - 1) / 2 + i && j >= (m - 1) / 2 - i) {
+      str += "*";
+    } else {
+      str += " ";
+    }
+  }
+  console.log(str);
+}
